4bd94a6d27c0bac68550553c93d0bb16a36ecf0a
Vitaly Koulakov <vitaly.koulakov@firstlinesoftware.com>
2015-06-01 19:41:38 +0300

    expression

--- src/main/scala/org/thymeleaf/play/expression/PlayExpressionParser.scala
+++ src/main/scala/org/thymeleaf/play/expression/PlayExpressionParser.scala
@@ -1,8 +1,9 @@
 package org.thymeleaf.play.expression
 
+import ognl.{OgnlContext, BooleanExpression}
 import org.thymeleaf.Configuration
 import org.thymeleaf.context.IProcessingContext
-import org.thymeleaf.standard.expression.{IStandardExpression, IStandardExpressionParser}
+import org.thymeleaf.standard.expression.{BooleanTokenExpression, VariableExpression, IStandardExpression, IStandardExpressionParser}
 
 import scala.util.parsing.combinator.JavaTokenParsers
 
@@ -12,13 +13,13 @@ import scala.util.parsing.combinator.JavaTokenParsers
  */
 class PlayExpressionParser extends JavaTokenParsers with IStandardExpressionParser{
 
-  def method : Parser[String] = "[\\w\\.]+".r ^^{ _.toString }
+  def boolean : Parser[IStandardExpression] = ("true" | "false") ^^ { s => new BooleanTokenExpression(s.toString) }
 
-  def link = "^@{" ~ method ~ "}" ^^ {
-     case "@{" ~ s ~ "}" => new LinkExpression(s.toString)
-  }
+  def variable : Parser[IStandardExpression] = """[\\w\\.]+""".r ^^{ s => new VariableExpression( s.toString ) }
 
-  override def parseExpression(configuration: Configuration, processingContext: IProcessingContext, input: String): IStandardExpression = parseAll(link, input) match {
+//  def link : Parser[IStandardExpression] = """^@{""" ~ method ~ """}""".r ^^ { s => new LinkExpression(s.toString) }
+
+  override def parseExpression(configuration: Configuration, processingContext: IProcessingContext, input: String): IStandardExpression = parseAll(boolean, input) match {
     case Success(result, _) => result
     case NoSuccess(msg, next) =>  throw new IllegalArgumentException(
       "Could not parse '" + input + "' near '" + next.pos.longString + ": " + msg)
--- src/test/scala/org/thymeleaf/play/expression/PlayExpressionParserSpec.scala
+++ src/test/scala/org/thymeleaf/play/expression/PlayExpressionParserSpec.scala
@@ -1,11 +1,16 @@
 package org.thymeleaf.play.expression
 
 import org.scalatest.{FlatSpec, Matchers}
+import org.thymeleaf.standard.expression.BooleanTokenExpression
 
 /**
  * Created by vitaly on 16.05.15.
  */
 class PlayExpressionParserSpec extends FlatSpec with Matchers{
+
+  "The parser" should "parse boolean" in {
+    assert(new PlayExpressionParser().parseExpression(null, null, "true") == new BooleanTokenExpression("true"))
+  }
 
   "The parser" should "parse a method" in {
     assert(new PlayExpressionParser().parseExpression(null, null, "routes.Application.index") == "routes.Application.index")
