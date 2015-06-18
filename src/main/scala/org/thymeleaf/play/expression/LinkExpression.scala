41a82ea5d7d50b6a0d8c6cf14f0ca1ce59627f18
vitaly <vitaly.koulakov@gmail.com>
2015-06-19 01:58:55 +0300

    play expr parser

--- src/main/scala/org/thymeleaf/play/expression/LinkExpression.scala
+++ src/main/scala/org/thymeleaf/play/expression/LinkExpression.scala
@@ -4,6 +4,7 @@ import org.thymeleaf.Configuration
 import org.thymeleaf.context.IProcessingContext
 import org.thymeleaf.standard.expression._
 
+
 /**
  * @author VKoulakov
  * @since 20.03.2015 19:32
@@ -11,7 +12,12 @@ import org.thymeleaf.standard.expression._
 case class LinkExpression (value: RouteExpression) extends IStandardExpression{
   override def getStringRepresentation: String = "@" + value
 
-  override def execute(configuration: Configuration, processingContext: IProcessingContext): AnyRef = ???
+  override def execute(configuration: Configuration, processingContext: IProcessingContext): AnyRef = {
+//    val cl = Class.forName("controllers.routes.Assets")
+//    cl.getMethod("at", classOf[String], classOf[String])
+    "/stylesheets/main.css"
+//    controllers.routes.Assets.at("stylesheets", "main.css")
+  }
 
   override def execute(configuration: Configuration, processingContext: IProcessingContext, expContext: StandardExpressionExecutionContext): AnyRef = ???
 }
