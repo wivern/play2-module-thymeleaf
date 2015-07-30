96e694c0deff2546e11eee7b35e32304a0e8964a
vitaly <vitaly.koulakov@gmail.com>
2015-07-30 08:03:11 +0300

    apache license

--- src/main/scala/org/thymeleaf/play/expression/RouteExpression.scala
+++ src/main/scala/org/thymeleaf/play/expression/RouteExpression.scala
@@ -1,3 +1,19 @@
+/*
+Copyright 2015 Vitaly Koulakov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package org.thymeleaf.play.expression
 
 
@@ -7,9 +23,6 @@ import org.thymeleaf.standard.expression.{StandardExpressionExecutionContext, IS
 import scala.reflect.runtime.{universe => ru}
 import play.core.Router
 
-/**
- * Created by vitaly on 08.06.15.
- */
 case class RouteExpression(route : String, params : List[IStandardExpression] = null) extends IStandardExpression{
 
   override def execute(configuration: Configuration, processingContext: IProcessingContext): AnyRef = {
--- src/test/scala/org/thymeleaf/play/expression/PlayExpressionParserSpec.scala
+++ src/test/scala/org/thymeleaf/play/expression/PlayExpressionParserSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Vitaly Koulakov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package org.thymeleaf.play.expression
 
 import org.scalatest.{FlatSpec, Matchers}
@@ -9,17 +25,6 @@ class PlayExpressionParserSpec extends FlatSpec with Matchers{
     assert(new PlayExpressionParser().parseExpression(null, null, "true") == new ThBooleanExpression(true))
   }
 
-
-/*
-  "The parser" should "parse a method" in {
-
-    assert(new PlayExpressionParser().parseExpression(null, null, "routes.Application.index") == "routes.Application.index")
-  } */
-
-/*  "The parser" should "parse a parens" in {
-    val parsed = new PlayExpressionParser().parseExpression(null, null, "(\"aa\",\"bb\")").asInstanceOf[ExpressionListExpression]
-    assert(parsed.list == ExpressionListExpression(List(new StringLiteralExpression("\"aa\""), new StringLiteralExpression("\"bb\""))).list)
-  }*/
 
   "The parser" should "parse a link" in {
     assert(new PlayExpressionParser().parseExpression(null, null, "@routes.Application.index1")  == RouteExpression("@routes.Application.index1"))
